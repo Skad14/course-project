265ca731a4ddaa5d8db62a34c018ba8446c517d2
Rafael Franca <rafa-cross@hotmail.com>
2023-12-11 17:38:24 -0300

    criando as listas de itens

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://live.staticflickr.com/2395/2266080998_2ea1aa6b43_b.jpg')
+  ];
 
 }
